7c892583fa45e6c28b88657fa4e5e1705a107bae
Raj Kale <kale44772@gmail.com>
2021-04-29 18:09:44 +0530

    routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import {lazy,Suspense} from 'react'
+import * as ROUTES from './constants/routes';
+const Login=lazy(()=>import('./pages/login'))
 
 function App() {
-
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Suspense fallback={<h1>Loading Data</h1>}>
+        <Switch>
+            <Route path={ROUTES.LOGIN} component={Login}/>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
